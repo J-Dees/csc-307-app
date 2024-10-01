1177e0dfa8b8f2115a7c8131215054f9046e4633
J-Dees <jdees@outlook.com>
2024-10-01 11:04:11 -0700

    added backend to new branch

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,17 @@
+// backend.js
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(port, () => {
+    console.log(
+        `Exmaple app listening at http://localhost:${port}`
+    );
+});
